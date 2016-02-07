d7c5a82648e6ec5c9a02ee62d2c6a8eed9592b22
HailMoisten <kirisame26@gmail.com>
2016-02-07 21:38:23 +0900

    Make ManhattanDamageField.cs and prefab.

---
+++ Utility/ManhattanDamageField.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManhattanDamageField : MonoBehaviour {
+    private int attackDamage = 10;
+    private int magicDamage = 0;
+    private Vector3 center = new Vector3(0, 0, 0);
+    private int manhattanSize = 1;
+    private float duration = 1.0f;
+
+    public void SetParamAndAwake(int ad, int md, Vector3 centerpos, int msize, float dur)
+    {
+        attackDamage = ad; magicDamage = md;
+        center = centerpos; manhattanSize = msize;
+        duration = dur;
+        transform.position = center;
+        transform.localScale = new Vector3(msize, 0, msize);
+        GetComponent<BoxCollider>().gameObject.SetActive(true);
+        Destroy(gameObject, duration);
+    }
+    
+    void OnTriggerEnter(Collider colliderInfo)
+    {
+        if (colliderInfo.gameObject.tag == "Animal" || colliderInfo.gameObject.tag == "Player")
+        {
+            Debug.Log("hit");
+            colliderInfo.gameObject.GetComponent<AAnimal>().TakeDamage(attackDamage, magicDamage);
+        }
+    }
+}
