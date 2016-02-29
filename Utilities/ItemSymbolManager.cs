bc71092ba728e22e223bab39a3a1c1b92f32fd6b
HailMoisten <kirisame26@gmail.com>
2016-02-29 22:00:19 +0900

    Add gravity to ItemSymbolManager

--- Utilities/ItemSymbolManager.cs
+++ Utilities/ItemSymbolManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class ItemSymbolManager : MonoBehaviour {
+    bool usedGravity = false;
     void Awake()
     {
         gameObject.layer = LayerMask.NameToLayer("Item");
@@ -9,10 +10,22 @@ public class ItemSymbolManager : MonoBehaviour {
     int step = 30;
 	// Update is called once per frame
 	void Update () {
+        if (usedGravity) { }
+        else
+        {
+            Vector3 surface = transform.position + new Vector3(0, Terrain.activeTerrain.SampleHeight(transform.position) - transform.position.y, 0);
+            iTween.MoveTo(gameObject,
+                iTween.Hash("position", surface,
+                "time", 2.0f,
+                "easetype", "linear"
+                ));
+            usedGravity = true;
+        }
+
         transform.rotation = Quaternion.Euler(
         0,
         Time.time * this.step,
         0
-    );
+        );
     }
 }
