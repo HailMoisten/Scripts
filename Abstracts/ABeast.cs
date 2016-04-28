e69ee08721c722ac7516684a99f758109f9e8201
HailMoisten <kirisame26@gmail.com>
2016-04-28 18:05:32 +0900

    Making ABeast.cs.

--- Abstracts/ABeast.cs
+++ Abstracts/ABeast.cs
@@ -2,11 +2,24 @@
 using System.Collections;
 
 public abstract class ABeast : AAnimal {
+    public override void YouDied()
+    {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    }
+    public override void GainExperience(int gainp)
+    {
+    }
+    protected override void SetDirection()
+    {
+    }
+    protected override void SettargetPOS(int n, bool focustarget)
+    {
+    }
+
+    public override void Awake () {
+        base.Awake();
+        gameObject.tag = "Beast";
+    }
 	
 	// Update is called once per frame
 	void Update () {
